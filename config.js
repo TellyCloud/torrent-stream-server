951e652afaa857c10f7f61fb5dbd47fe76cf7d17
TellyCloud <156745846+TellyCloud@users.noreply.github.com>
2025-09-03 16:58:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 module.exports = {
-  PORT: process.env.PORT || 3000,
-  JWT_SECRET: process.env.JWT_SECRET || "supersecretkey",
-  REQUIRE_AUTH: process.env.REQUIRE_AUTH === "true"
+  PORT: Number(process.env.PORT || 3000),
+  REQUIRE_AUTH: (process.env.REQUIRE_AUTH === 'true'),
+  JWT_SECRET: process.env.JWT_SECRET || 'supersecretkey',
+  TORRENT_TMP_DIR: process.env.TORRENT_TMP_DIR || './tmp',
+  TTR_CLEANUP_MINUTES: Number(process.env.TTR_CLEANUP_MINUTES || 5)
 }
